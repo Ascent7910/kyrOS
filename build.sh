103ad8e6b0afbaea5a48402fdef76674093ff3f4
Max <118260621+Ascent7910@users.noreply.github.com>
2024-12-20 17:40:55 -0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -13,7 +13,8 @@ RELEASE="$(rpm -E %fedora)"
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install steam
+dnf5 -y install steam
+dnf5 clean all
 
 #### Example for enabling a System Unit File
 
