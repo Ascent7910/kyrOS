7874968fb98ee9345d400c1f16067b568b335f9a
Max <github.b0o20@passmail.net>
2024-12-29 19:58:49 -0800

    added -y

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ RELEASE="$(rpm -E %fedora)"
 ### Install packages
 
 dnf5 remove -y firefox
-dnf5 group remove "KDE Applications"
+dnf5 group remove -y "KDE Applications"
 
 
 #### Example for enabling a System Unit File
