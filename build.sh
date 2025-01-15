38c67098d7000e9d8acdb0634873d9434a35ceea
Max <118260621+Ascent7910@users.noreply.github.com>
2025-01-14 21:43:34 -0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,10 +4,14 @@ set -ouex pipefail
 
 RELEASE="$(rpm -E %fedora)"
 
+### DNF Copr Repos
 
-### Install packages
+dnf5 copr enable ryanabx/cosmic-epoch 
+
+### DNF packages
 
 dnf5 remove -y firefox plasma-welcome plasma-discover-rpm-ostree
+dnf5 install -y cosmic-desktop
 
 
 #### Example for enabling a System Unit File
